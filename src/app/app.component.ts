d68ee7808687eaa06e85760f77c96600c24cfc1a
Narein Narayan Boddapati <boddapnn@mail.uc.edu>
2025-01-22 11:28:17 -0500

    changed styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';import { HeaderComponent } from './header/header.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, AddTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
